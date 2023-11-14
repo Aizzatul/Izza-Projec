a8f52b81d0fbb968a8b3dffc5805483fc3f33714
Aizzatul <148950116+Aizzatul@users.noreply.github.com>
2023-11-14 14:54:56 +0700

    Add files via upload
    Uts Aizzatul Adawiyah

---
+++ xixi/xaxa/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def xaxa(request):
+  template = loader.get_template('about.html')
+  return HttpResponse(template.render())
